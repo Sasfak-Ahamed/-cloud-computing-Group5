5464f276d9bfe6c4873144a5fca03a0ca07227bf
Sasfak-Ahamed <68736313+Sasfak-Ahamed@users.noreply.github.com>
2026-04-29 22:38:01 +0530

    Delete cloud-computing-g-cw-master directory

--- cloud-computing-g-cw-master/services/techsalary-frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  env: {
-    NEXT_PUBLIC_BFF_URL: 'http://4.188.102.12',
-  },
-}
-module.exports = nextConfig
